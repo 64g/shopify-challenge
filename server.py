55b1768a5e36064f3eb45f3599d3278d77ee4f34
64g <USERNAME@uwaterloo.ca>
2021-05-05 09:52:14 -0400

    simple image uploader

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
