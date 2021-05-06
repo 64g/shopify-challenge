3b1e44a89e1e55ffc8f3b3d3117c92436e98aef7
64g <USERNAME@uwaterloo.ca>
2021-05-06 10:26:16 -0400

    List images

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
+import imghdr
 import os
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
-# Security Config
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 4 * 1024 * 1024   # TODO: Add image size check
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
 app.config['UPLOAD_PATH'] = 'uploads'
 
 
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    files = os.listdir(app.config['UPLOAD_PATH'])  # List images
+    return render_template('index.html', files=files)
 
 
 @app.route('/', methods=['POST'])
@@ -21,10 +30,15 @@ def upload_files():
         filename = secure_filename(uploaded_file.filename)
         if filename != '':
             file_ext = os.path.splitext(filename)[1]
-            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(uploaded_file.stream):
                 abort(400)
             uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return redirect(url_for('index'))
+
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 
 if __name__ == "__main__":
